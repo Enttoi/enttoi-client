580b35243eafef349562c329c29a2f76fae3dda5
Jenya Y. <jenyay@gmail.com>
2015-10-30 15:58:46 +0200

    Led struct

---
+++ src/leds.py
@@ -0,0 +1,17 @@
+﻿import abc
+import RPi.GPIO as GPIO
+
+class Led(object):
+	
+	def __init__(self, identity, pin, color):
+		self.identity = identity
+		self.pin = pin
+		self.color = color
+		GPIO.setup(self.pin, GPIO.OUT)
+		self.off() # reset state
+
+	def on(self):
+		GPIO.output(self.pin,True)
+
+	def off(self):
+		GPIO.output(self.pin,False)
