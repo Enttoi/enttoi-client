7dd0c1e15200c84688d55cc7451dd662f260e8fe
Jenya Y <jenyay@gmail.com>
2016-02-07 21:48:10 +0200

    Single LED

--- src/client.py
+++ src/client.py
@@ -12,33 +12,20 @@ GPIO.setmode(GPIO.BCM)
 # wPi = 2, 3
 doors = [sensors.Sensor(1, 27, "cabin_door"), sensors.Sensor(2, 22, "cabin_door")]
 
-green_indicator = leds.Led(17) # wPi = 0
-yellow_indicator = leds.Led(18) # wPi = 1
-red_indicator = leds.Led(4) # wPi = 7
-indicators = [red_indicator, green_indicator, yellow_indicator]
+green_indicator = leds.Led(4) # wPi = 7
+indicators = [green_indicator]
  
 # indicates boot of the client
 def boot():
 	for i in indicators:
 		i.on()
-	time.sleep(1) 
-	for i in indicators:
-		i.off()
 
 # loops forever on each sensor and sends their state
 def start_sending():
 	while True:
 		for d in doors:
-			yellow_indicator.on()
-			success = d.send_state(client_token, end_point)		
-				
-			for i in indicators:
-				i.off()
-			if(success == False):
-				red_indicator.on()
-			else:
-				green_indicator.on()
-		
+			success = d.send_state(client_token, end_point)	
+            		
 		time.sleep(0.5)
 
 boot()
