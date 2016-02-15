b55bad4a8f5b520d0d6590ed402f964cc0dda97b
Jenya Y <jenyay@gmail.com>
2016-02-16 00:36:19 +0200

    Switching between blinking frequency

--- output.py
+++ output.py
@@ -2,15 +2,13 @@
 import abc
 import threading
 import RPi.GPIO as GPIO
-
-CONST_BLINK_FREQUENCY = 0.07
 	
 class Led(object):
-
 	
 	def __init__(self, pin):
 		self.pin = pin
 		GPIO.setup(self.pin, GPIO.OUT)
+		self.__blinking_frequency = -1 # indicates no blinking
 		self.off() # reset state
 
 	def on(self):
@@ -22,32 +20,35 @@ class Led(object):
 		GPIO.output(self.pin,False)
 		
 	def blink_fast(self):
-		self.blink(0.07)
+		self.blink(0.06)
 
 	def blink_slow(self):
-		self.blink(0.3)
+		self.blink(0.5)
 		
 	def blink(self, frequency):
-		self.__ensure_not_blinking()
-		self.__stop_blink_event = threading.Event()	
-		self.__thread = threading.Thread(target=self.__do_blinking, args = (frequency,))
-		self.__thread.daemon = True
-		self.__thread.start()
+		if self.__blinking_frequency == -1:
+			# no blinking currently => create worker for it
+			self.__stop_blink_event = threading.Event()	
+			self.__thread = threading.Thread(target=self.__do_blinking)
+			self.__thread.daemon = True
+			self.__thread.start()
+		
+		self.__blinking_frequency = frequency
 	
-	def __do_blinking(self, frequency):
+	def __do_blinking(self):
 		state = False;
 		GPIO.output(self.pin, state)
 	
 		while (not self.__stop_blink_event.is_set()):
 			state = not state
 			GPIO.output(self.pin,state)
-			self.__stop_blink_event.wait(frequency)
+			self.__stop_blink_event.wait(self.__blinking_frequency)
 			
 	def __ensure_not_blinking(self):
-		try:
-			self.__stop_blink_event
-		except (NameError, AttributeError):
-			pass
-		else:
-			self.__stop_blink_event.set()
-			self.__thread.join()
+		if self.__blinking_frequency == -1:
+			return
+			
+		self.__stop_blink_event.set()
+		self.__thread.join()
+		self.__blinking_frequency = -1
+		
