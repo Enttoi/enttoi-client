b17687317e6a8727d52742f1f9e10188b0e16237
Jenya Y <jenyay@gmail.com>
2016-02-16 00:03:11 +0200

    In pascal casing in naming methods

--- output.py
+++ output.py
@@ -14,21 +14,21 @@ class Led(object):
 		self.off() # reset state
 
 	def on(self):
-		self.__ensureNotBlinking()
+		self.__ensure_not_blinking()
 		GPIO.output(self.pin,True)
 
 	def off(self):
-		self.__ensureNotBlinking()
+		self.__ensure_not_blinking()
 		GPIO.output(self.pin,False)
 
 	def blink(self):
-		self.__ensureNotBlinking()
+		self.__ensure_not_blinking()
 		self.__stop_blink_event = threading.Event()	
-		self.__thread = threading.Thread(target=self.__doBlinking)
+		self.__thread = threading.Thread(target=self.__do_blinking)
 		self.__thread.daemon = True
 		self.__thread.start()
 		
-	def __doBlinking(self):
+	def __do_blinking(self):
 		state = False;
 		GPIO.output(self.pin, state)
 	
@@ -37,7 +37,7 @@ class Led(object):
 			GPIO.output(self.pin,state)
 			self.__stop_blink_event.wait(CONST_BLINK_FREQUENCY)
 			
-	def __ensureNotBlinking(self):
+	def __ensure_not_blinking(self):
 		try:
 			self.__stop_blink_event
 		except (NameError, AttributeError):
